7c957e2ba711fb0cad83917de70016f3f9b96282
louison <iwa-30@sr.dmcs.pl>
2023-03-13 15:00:59 +0100

    Api structure creation, add .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Data-Battle-DatAvengers
 Lithology scanner for [factpages.npd.no](https://factpages.npd.no/en/wellbore/PageView/Exploration/All)
+
+## Installation
+
+`pip install -r requirements.txt`  
+
+## Utilisation
+
+`uvicorn api.app:app`  
--- api/app.py
+++ api/app.py
@@ -1,2 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI,  UploadFile
 import os
+
+app = FastAPI()
+
+#If file is small we can use file : bytes
+@app.post("/api/uploadLithology")
+async def uploadLithology(file : UploadFile):
+    #TODO
+    return {"filename" : file.filename}
+
+@app.post("/api/extractColumn")
+async def extractColumn(x_min : int, y_min : int, x_max : int, y_max : int):
+    #TODO
+    return {'array': [x_min, y_min, x_max, y_max]}
+
+@app.get("/api/lithology")
+async def getLithology():
+    return 0
